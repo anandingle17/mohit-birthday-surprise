86c272d59559994c566ab3f4b80b0b94d41e5f82
anandingle17 <anandingle809@gmail.com>
2026-09-02 22:34:39 +0530

    Update memories section

--- src/config/birthdayConfig.js
+++ src/config/birthdayConfig.js
@@ -50,28 +50,28 @@ export const birthdayConfig = {
   photos: [
     {
       id: 1,
-      src: "public/photos/1 (2).jpeg",
+      src: "/photos/1 (2).jpeg",
       fallbackSrc: "https://images.unsplash.com/photo-1511632765486-a01980e01a18?auto=format&fit=crop&w=800&q=80",
       title: "Unforgettable Laughs",
       caption: "Every moment with you is a core memory! 😃"
     },
     {
       id: 2,
-      src: "public/photos/2 (2).jpeg",
+      src: "/photos/2 (2).jpeg",
       fallbackSrc: "https://images.unsplash.com/photo-1529156069898-49953e39b3ac?auto=format&fit=crop&w=800&q=80",
       title: "un expected meetups",
       caption: "To more fun! 💨"
     },
     {
       id: 3,
-      src: "public/photos/3 (2).jpeg",
+      src: "/photos/3 (2).jpeg",
       fallbackSrc: "https://images.unsplash.com/photo-1517457373958-b7bdd4587205?auto=format&fit=crop&w=800&q=80",
       title: "Celebration Vibe",
       caption: "Cheers to another year of pure greatness! 🥂✨"
     },
     {
       id: 4,
-      src: "public/photos/4 (2).jpeg",
+      src: "/photos/4 (2).jpeg",
       fallbackSrc: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=800&q=80",
       title: "Good Times",
       caption: "Brothers for life through thick and thin! 🤝🔥"
